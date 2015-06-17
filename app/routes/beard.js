7dce54c3b5e336528ba5dbe154160de120c3a1d1
ian & alex <student@epicodus.com>
2015-06-17 16:23:59 -0700

    Hide forms on leaving route.

--- app/routes/beard.js
+++ app/routes/beard.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function (params) {
     return this.store.find('beard', params.beard_id);
-  }
+  },
+  hideForm: function () {
+    var controller = this.get('controller');
+    controller.set('newReview', false);
+  }.on('deactivate')
 });
--- app/routes/beards.js
+++ app/routes/beards.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function () {
     return this.store.find('beard');
-  }
+  },
+  hideForm: function () {
+    var controller = this.get('controller');
+    controller.set('newBeard', false);
+  }.on('deactivate')
 });
